9208725c38d4e944a0efa43ebec9d75349ba7496
Nolan Picini <nolanpic@gmail.com>
2020-01-16 14:20:00 -0800

    Add buttons in Dashboard component

--- app/src/components/Dashboard.js
+++ app/src/components/Dashboard.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Dashboard = props => {
+const StyledDashboard = styled.div`
+    width: 35%;
+    display: flex;
+    justify-content: space-around;
+
+    button {
+        padding: 1rem 2rem;
+        cursor: pointer;
+        font-size: 1.4rem;
+    }
+`;
+
+const Dashboard = ({ setBalls, setStrikes }) => {
     return (
-        <>
-            Dashboard
-        </>
+        <StyledDashboard>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>Hit</button>
+        </StyledDashboard>
     );
 };
 
