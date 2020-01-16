e45366ee914d2e9f72dcc40b7515df7a17049a42
Nolan Picini <nolanpic@gmail.com>
2020-01-16 14:07:09 -0800

    Set up initial state

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Display from './components/Display';
 import Dashboard from './components/Dashboard';
 
 function App() {
+
+  const [ balls, setBalls ] = useState(0);
+  const [ strikes, setStrikes ] = useState(0);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <Display />
-        <Dashboard />
+        <Dashboard setBalls={setBalls} setStrikes={setStrikes} />
       </header>
     </div>
   );
